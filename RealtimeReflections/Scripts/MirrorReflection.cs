eacbb3656f20e469c11ed9ff03b548426f465725
Nystul-the-Magician <11569250+Nystul-the-Magician@users.noreply.github.com>
2018-06-24 19:06:26 +0200

    fixes after unintended commit changes

--- RealtimeReflections/Scripts/MirrorReflection.cs
+++ RealtimeReflections/Scripts/MirrorReflection.cs
@@ -118,7 +118,7 @@ namespace RealtimeReflections
 		    //Matrix4x4 projection = cam.CalculateObliqueMatrix(clipPlane);
 		    //reflectionCamera.projectionMatrix = projection; // do not set oblique projection matrix since it will fuck up fog in reflections - disabling this step seems to do not any harm ;)
 
-            reflectionCamera.cullingMask = ~(1 << 4) & m_ReflectLayers.value; //LayerMask.NameToLayer("Everything"); //m_ReflectLayers.value; // never render water layer
+            reflectionCamera.cullingMask = ~(1 << 4) & LayerMask.NameToLayer("Everything"); //m_ReflectLayers.value; // never render water layer
             //reflectionCamera.layerCullDistances = layerCullDistances;
 		    reflectionCamera.targetTexture = m_ReflectionTexture;
 
